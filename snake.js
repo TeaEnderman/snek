eb4f2324d6f6e7594788942446d68f4c9f4d85fa
TeaEnderman <132008060+TeaEnderman@users.noreply.github.com>
2023-12-02 18:25:31 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -67,12 +67,14 @@ function update() {
     if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
         gameOver = true;
         window.location = "https://youtu.be/dQw4w9WgXcQ?si=Y1mQmuErCzrmFKte";
+        alert("Fuck you. You suck.");
     }
 
     for (let i = 0; i < snakeBody.length; i++) {
         if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
             gameOver = true;
             window.location = "https://youtu.be/dQw4w9WgXcQ?si=Y1mQmuErCzrmFKte";
+            alert("Fuck you. You suck.");
         }
     }
 }
